f47c2e75e4248d16db40a7ef893a68b936c8e53d
Reversep <adilet.t.k@gmail.com>
2024-08-25 23:22:03 +0600

    added categories func

--- django-project/web/urls.py
+++ django-project/web/urls.py
@@ -6,4 +6,10 @@ urlpatterns = [
     path('products/create/', views.product_create, name='product_create'),
     path('products/<int:pk>/update/', views.product_update, name='product_update'),
     path('products/<int:pk>/delete/', views.product_delete, name='product_delete'),
+
+    path('categories/', views.category_list, name='category_list'),
+    path('categories/create/', views.category_create, name='category_create'),
+    path('categories/<int:pk>/update/', views.category_update, name='category_update'),
+    path('categories/<int:pk>/delete/', views.category_delete, name='category_delete'),
 ]
+
--- django-project/web/views.py
+++ django-project/web/views.py
@@ -20,20 +20,24 @@ def product_create(request):
     if request.method == 'POST':
         name = request.POST['name']
         price = request.POST['price']
+        category_id = request.POST['category']
+
+        category = get_object_or_404(Category, id=category_id)
 
         product = Product.objects.create(name=name, price=price)
+        product.categories.add(category)
 
         channel_layer = get_channel_layer()
         async_to_sync(channel_layer.group_send)(
             'notifications',
             {
                 'type': 'send_notification',
-                'message': f'Новый продукт "{product.name}" был создан!'
+                'message': f'Новый продукт "{product.name}" в категории "{category.name}" был создан!'
             }
         )
 
         return JsonResponse(
-            {'success': True, 'product_name': product.name, 'product_price': product.price, 'product_id': product.id})
+            {'success': True, 'product_name': product.name, 'product_price': product.price, 'product_id': product.id, 'category_name': category.name, 'category_id': category.id})
 
     return JsonResponse({'success': False})
 
@@ -44,6 +48,10 @@ def product_update(request, pk):
         old_name = product.name
         product.name = request.POST['name']
         product.price = request.POST['price']
+        category_id = request.POST['category']
+
+        category = get_object_or_404(Category, id=category_id)
+        product.categories.set([category])
         product.save()
 
         channel_layer = get_channel_layer()
@@ -51,12 +59,12 @@ def product_update(request, pk):
             'notifications',
             {
                 'type': 'send_notification',
-                'message': f'Продукт "{old_name}" был обновлён на "{product.name}"!'
+                'message': f'Продукт "{old_name}" был обновлён на "{product.name}" в категории "{category.name}"!'
             }
         )
 
         return JsonResponse(
-            {'success': True, 'product_id': product.id, 'product_name': product.name, 'product_price': product.price})
+            {'success': True, 'product_id': product.id, 'product_name': product.name, 'product_price': product.price, 'category_name': category.name, 'category_id': category.id})
 
     return JsonResponse({'success': False})
 
@@ -77,5 +85,70 @@ def product_delete(request, pk):
         )
 
         return JsonResponse({'success': True, 'product_id': pk})
+
+    return JsonResponse({'success': False})
+
+def category_list(request):
+    categories = Category.objects.all()
+    return render(request, 'categories.html', {'categories': categories})
+
+
+def category_create(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+
+        category = Category.objects.create(name=name)
+
+        channel_layer = get_channel_layer()
+        async_to_sync(channel_layer.group_send)(
+            'notifications',
+            {
+                'type': 'send_notification',
+                'message': f'Новая категория "{category.name}" была создана!'
+            }
+        )
+
+        return JsonResponse({'success': True, 'category_name': category.name, 'category_id': category.id})
+
+    return JsonResponse({'success': False})
+
+
+def category_update(request, pk):
+    category = get_object_or_404(Category, pk=pk)
+    if request.method == 'POST':
+        old_name = category.name
+        category.name = request.POST['name']
+        category.save()
+
+        channel_layer = get_channel_layer()
+        async_to_sync(channel_layer.group_send)(
+            'notifications',
+            {
+                'type': 'send_notification',
+                'message': f'Категория "{old_name}" была обновлена на "{category.name}"!'
+            }
+        )
+
+        return JsonResponse({'success': True, 'category_id': category.id, 'category_name': category.name})
+
+    return JsonResponse({'success': False})
+
+
+def category_delete(request, pk):
+    category = get_object_or_404(Category, pk=pk)
+    if request.method == 'POST':
+        category_name = category.name
+        category.delete()
+
+        channel_layer = get_channel_layer()
+        async_to_sync(channel_layer.group_send)(
+            'notifications',
+            {
+                'type': 'send_notification',
+                'message': f'Категория "{category_name}" была удалена!'
+            }
+        )
+
+        return JsonResponse({'success': True, 'category_id': pk})
 
     return JsonResponse({'success': False})
